731316e984899f7b78b2ecd15d13eff93dd0b6b0
yaming.feng <yaming.feng@vhall.com>
2024-09-17 18:20:33 +0800

    feat: english ocr

--- tal-ai/composition/main.go
+++ tal-ai/composition/main.go
@@ -2,29 +2,22 @@ package main
 
 import (
 	"bytes"
-	"crypto/hmac"
-	"crypto/sha1"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
-	"maps"
 	"net/http"
-	"net/url"
 	"os"
-	"slices"
-	"strings"
-	"time"
-
-	"github.com/google/uuid"
 )
 
 // 手写识别
 
 const (
-	API_URL = "http://openai.100tal.com/aiimage/cn-composition"
-	IMG_URL = "../imgs/pic_fangge.jpg"
+	CN_COMPOSITION_URL = "http://openai.100tal.com/aiimage/cn-composition"
+	EN_OCR_URL         = "http://openai.100tal.com/aiocr/english-ocr"
+	IMG_URL            = "../imgs/pic_fangge.jpg"
+	EN_IMG_URL         = "../imgs/en.jpg"
 )
 
 var (
@@ -33,23 +26,41 @@ var (
 )
 
 func main() {
+	EnOcr()
+}
+
+// EnOcr 英文 OCR
+func EnOcr() {
+	params := map[string]any{
+		//"image_url": IMG_URL,
+		"image_base64": getBase64Image(EN_IMG_URL),
+		"rotate":       0, // 是否旋转
+		"enhanced":     0, // 是否多次识别
+	}
+	urlParams := createSign(APP_SECRET, params, nil)
+	body := httpPost(EN_OCR_URL+"?"+urlParams, params)
+	fmt.Printf("%+v\n", string(body))
+}
+
+// CnComposition 中文手写识别
+func CnComposition() {
 	if APP_KEY == "" || APP_SECRET == "" {
 		log.Fatal("APP_KEY or APP_SECRET is empty")
 	}
 	params := map[string]any{
 		//"image_url":           IMG_URL,
-		"image_base64":        getBase64Image(),
+		"image_base64":        getBase64Image(IMG_URL),
 		"details":             false,
 		"paragraph_detection": true,
 	}
 	urlParams := createSign(APP_SECRET, params, nil)
 
-	body := httpPost(API_URL+"?"+urlParams, params)
+	body := httpPost(CN_COMPOSITION_URL+"?"+urlParams, params)
 	_ = os.WriteFile("result.json", body, 0644)
 }
 
-func getBase64Image() string {
-	content, err := os.ReadFile(IMG_URL)
+func getBase64Image(imgPath string) string {
+	content, err := os.ReadFile(imgPath)
 	if err != nil {
 		panic(err)
 	}
@@ -68,68 +79,4 @@ func httpPost(url string, params map[string]any) []byte {
 	defer resp.Body.Close()
 	body, _ := io.ReadAll(resp.Body)
 	return body
-}
-
-func createSign(secret string, body map[string]any, urlParams map[string]string) string {
-	bodyJson, err := json.Marshal(body)
-	if err != nil {
-		panic(err)
-	}
-
-	signParams := map[string]string{
-		"access_key_id":   APP_KEY,
-		"timestamp":       time.Now().Format("2006-01-02T15:04:05"),
-		"signature_nonce": uuid.NewString(),
-		"request_body":    string(bodyJson),
-	}
-
-	for k, v := range urlParams {
-		signParams[k] = v
-	}
-
-	signature, err := GenerateSignature(signParams, secret)
-	if err != nil {
-		panic(err)
-	}
-
-	delete(signParams, "request_body")
-	signParams["signature"] = url.QueryEscape(signature)
-	fmt.Println("signature:", signParams["signature"])
-
-	return urlFormat(signParams)
-}
-
-// GenerateSignature 用于生成签名
-func GenerateSignature(parameters map[string]string, accessKeySecret string) (string, error) {
-	// 计算证书签名
-	stringToSign := urlFormat(parameters)
-
-	// 进行base64 encode
-	secret := accessKeySecret + "&"
-	h := hmac.New(sha1.New, []byte(secret))
-	if _, err := h.Write([]byte(stringToSign)); err != nil {
-		return "", err
-	}
-	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
-	return strings.TrimRight(signature, "\n"), nil
-}
-
-// urlFormat 将 map 参数转换为 url 参数格式
-func urlFormat(params map[string]string) string {
-	if len(params) == 0 {
-		return ""
-	}
-	var buf strings.Builder
-	keys := slices.Collect(maps.Keys(params))
-	slices.Sort(keys)
-	for _, k := range keys {
-		v := params[k]
-		if buf.Len() > 0 {
-			buf.WriteByte('&')
-		}
-		buf.WriteString(k)
-		buf.WriteByte('=')
-		buf.WriteString(v)
-	}
-	return buf.String()
 }
---
+++ tal-ai/composition/sign.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"maps"
+	"net/url"
+	"slices"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func createSign(secret string, body map[string]any, urlParams map[string]string) string {
+	bodyJson, err := json.Marshal(body)
+	if err != nil {
+		panic(err)
+	}
+
+	signParams := map[string]string{
+		"access_key_id":   APP_KEY,
+		"timestamp":       time.Now().Format("2006-01-02T15:04:05"),
+		"signature_nonce": uuid.NewString(),
+		"request_body":    string(bodyJson),
+	}
+
+	for k, v := range urlParams {
+		signParams[k] = v
+	}
+
+	signature, err := GenerateSignature(signParams, secret)
+	if err != nil {
+		panic(err)
+	}
+
+	delete(signParams, "request_body")
+	signParams["signature"] = url.QueryEscape(signature)
+	fmt.Println("signature:", signParams["signature"])
+
+	return urlFormat(signParams)
+}
+
+// GenerateSignature 用于生成签名
+func GenerateSignature(parameters map[string]string, accessKeySecret string) (string, error) {
+	// 计算证书签名
+	stringToSign := urlFormat(parameters)
+
+	// 进行base64 encode
+	secret := accessKeySecret + "&"
+	h := hmac.New(sha1.New, []byte(secret))
+	if _, err := h.Write([]byte(stringToSign)); err != nil {
+		return "", err
+	}
+	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
+	return strings.TrimRight(signature, "\n"), nil
+}
+
+// urlFormat 将 map 参数转换为 url 参数格式
+func urlFormat(params map[string]string) string {
+	if len(params) == 0 {
+		return ""
+	}
+	var buf strings.Builder
+	keys := slices.Collect(maps.Keys(params))
+	slices.Sort(keys)
+	for _, k := range keys {
+		v := params[k]
+		if buf.Len() > 0 {
+			buf.WriteByte('&')
+		}
+		buf.WriteString(k)
+		buf.WriteByte('=')
+		buf.WriteString(v)
+	}
+	return buf.String()
+}
